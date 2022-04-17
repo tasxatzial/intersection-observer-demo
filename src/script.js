e7e2aff7e75435076ec1d285125f884dd7924097
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-04-17 14:16:20 +0300

    change header style and padding when scrolled past the intro section heading

--- src/script.js
+++ src/script.js
@@ -1,12 +1,13 @@
 const header = document.querySelector('header');
-const introObserved = document.getElementById('intro-observed');
+const introObserved = document.querySelector('h1');
+const introSection = document.getElementById('intro');
 
 function intersectionObserverFn(entries, introObserver) {
     for (let entry of entries) {
         if (!entry.isIntersecting) {
-            ;
+            header.classList.add('scrolled');
         } else {
-            ;
+            header.classList.remove('scrolled')
         }
     }
 }
@@ -19,7 +20,8 @@ const headerResizeObserver = new ResizeObserver(entries => {
             introObserver.unobserve(header);
         }
         const headerHeight = entry.contentRect.top + entry.contentRect.bottom;
-        const options = {threshold: 1, rootMargin: -headerHeight + "px 0px 0px 0px"};
+        const introSectionHeight = introSection.getBoundingClientRect().height;
+        const options = {threshold: 1, rootMargin: -headerHeight + 'px 0px ' + introSectionHeight + 'px 0px'};
         introObserver = new IntersectionObserver(intersectionObserverFn, options);
         introObserver.observe(introObserved);
     }
