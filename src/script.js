cbd960b54d7fa1e26392398d5ff1af4dc453ff25
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-04-18 03:55:48 +0300

    minor adjustments & fixes

--- src/script.js
+++ src/script.js
@@ -1,14 +1,13 @@
 const header = document.querySelector('header');
 const h1 = document.querySelector('h1');
-const introSection = document.getElementById('intro');
 const navList = document.querySelector('.nav-list');
 
 function changeHeader(entries, introObserver) {
     for (let entry of entries) {
-        if (!entry.isIntersecting) {
-            header.classList.add('scrolled');
-        } else {
+        if (entry.isIntersecting) {
             header.classList.remove('scrolled');
+        } else {
+            header.classList.add('scrolled');
         }
     }
 }
@@ -20,9 +19,8 @@ const navList_RO = new ResizeObserver(entries => {
         if (h1_IO) {
             h1_IO.unobserve(h1);
         }
-        const headerHeight = header.getBoundingClientRect().height; 
-        const introSectionHeight = introSection.getBoundingClientRect().height;
-        const options = {threshold: 1, rootMargin: -headerHeight + 'px 0px ' + introSectionHeight + 'px 0px'};
+        const headerHeight = header.getBoundingClientRect().height;
+        const options = {threshold: 1, rootMargin: -headerHeight + 'px 0px 0px 0px'};
         h1_IO = new IntersectionObserver(changeHeader, options);
         h1_IO.observe(h1);
     }
