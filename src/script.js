bf6abdd9cdc40935b0a80664c04f7ee5982fdc1a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-04-18 05:50:03 +0300

    underlink the nav link that corresponds to the current visible section

--- src/script.js
+++ src/script.js
@@ -1,6 +1,13 @@
 const header = document.querySelector('header');
 const h1 = document.querySelector('h1');
 const navList = document.querySelector('.nav-list');
+const homeSection = document.getElementById('intro');
+const historySection = document.getElementById('history');
+const gallerySection = document.getElementById('gallery');
+
+const homeLink = navList.querySelector('[href="#intro"]');
+const historyLink = navList.querySelector('[href="#history"');
+const galleryLink = navList.querySelector('[href="#gallery"');
 
 function changeHeader(entries, introObserver) {
     for (let entry of entries) {
@@ -27,3 +34,35 @@ const navList_RO = new ResizeObserver(entries => {
 })
 
 navList_RO.observe(navList);
+
+const sections_IO_options = {
+    rootMargin: '-49.99% 0px -50% 0px'
+}
+
+let currentLink = null;
+
+const sections_IO = new IntersectionObserver(entries => {
+    for (let entry of entries) {
+        if (entry.isIntersecting) {
+            let id = entry.target.getAttribute('id');
+            if (currentLink != null) {
+                currentLink.classList.remove('current-link');
+            }
+            if (id === 'intro') {
+                homeLink.classList.add('current-link');
+                currentLink = homeLink;
+            } else if (id === 'history') {
+                historyLink.classList.add('current-link');
+                currentLink = historyLink;
+            } else {
+                galleryLink.classList.add('current-link');
+                currentLink = galleryLink;
+            }
+        }
+    }
+},
+sections_IO_options);
+
+sections_IO.observe(homeSection)
+sections_IO.observe(historySection)
+sections_IO.observe(gallerySection)
