10f27ce0ba832cd8910daeced3976a8985ef2ba9
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-06-13 16:22:43 +0300

    refactoring js

--- src/js/script.js
+++ src/js/script.js
@@ -5,79 +5,45 @@ const navList = nav.querySelector('.nav-list');
 const navLinks = nav.querySelectorAll('.nav-link');
 const navBtn = header.querySelector('.toggle-nav');
 const destinations = document.querySelectorAll('.destination');
-const images = document.querySelectorAll('.destination-img');
+const destinationPhotos = document.querySelectorAll('.destination-img');
 const destinationBodies = document.querySelectorAll('.destination-body');
 const introContent = document.querySelector('.intro-content');
 const historyContent = document.querySelector('.history-content');
 const destinationsContent = document.querySelector('.destinations-content');
-const mqList = window.matchMedia("(min-width: 45rem)");
+const mqList = window.matchMedia("(min-width: 45em)");
 
+(function() {
+    if (mqList.addEventListener) {
+        mqList.addEventListener('change', closeNavOnResize);
+    } else {
+        mqList.addListener(closeNavOnResize);
+    }
+    window.addEventListener('click', autoCloseNav);
+    if (supportsObservers()) {
+        createH1Observers();
+        createSectionObservers();
+        createDestinationInfoObservers();
+        createDestinationPhotoObservers();
+    } else {
+        //load all photos
+        for (let i = 0; i < destinationPhotos.length; i++) {
+            destinationPhotos[i].src = destinationPhotos[i].getAttribute('data-src');
+            destinationPhotos[i].classList.add('js-loaded-img');
+        }
 
-function changeHeader(entries, observer) {
-    for (let i = 0; i < entries.length; i++) {
-        if (entries[i].isIntersecting) {
-            if (!header.classList.contains('js-nav-open')) {
-                header.classList.add('js-bg-transparent');
-            }
-            header.classList.remove('box-shadow');
-            header.classList.add('js-above-h1');
-        } else {
-            if (!header.classList.contains('js-nav-open')) {
-                header.classList.add('box-shadow');
-            }
-            header.classList.remove('js-bg-transparent');
-            header.classList.remove('js-above-h1');
+        //slide all text
+        for (let i = 0; i < destinationBodies.length; i++) {
+            destinationBodies[i].classList.add('js-slide-in');
         }
     }
-}
-
-function underlineCurrentLink(entries, observer) {
-    for (let i = 0; i < entries.length; i++) {
-        if (entries[i].isIntersecting) {
-            let id = entries[i].target.parentNode.id;
-            let currentUnderlined = navList.querySelector('.current-link');
-            let newUnderlined = navList.querySelector('[href="#' + id + '"]');
-            if (currentUnderlined) {
-                currentUnderlined.classList.remove('current-link');
-            }
-            newUnderlined.classList.add('current-link');
-        }
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].addEventListener('click', closeNav);
     }
-}
-
-function loadImg(entries, observer) {
-    for (let i = 0; i < entries.length; i++) {
-        if (entries[i].isIntersecting) {
-            let img = entries[i].target.querySelector('img');
-            img.src = img.getAttribute('data-src');
-            img.classList.add('js-loaded-img');
-            observer.unobserve(entries[i].target);
-        }
-    }
-}
-
-function slideText(entries, observer) {
-    for (let i = 0; i < entries.length; i++) {
-        if (entries[i].isIntersecting) {
-            let planetBody = entries[i].target.querySelector('.destination-body');
-            planetBody.classList.add('js-slide-in');
-            observer.unobserve(entries[i].target);
-        }
-    }
-}
-
-function loadAllImages() {
-    for (let i = 0; i < images.length; i++) {
-        images[i].src = images[i].getAttribute('data-src');
-        images[i].classList.add('js-loaded-img');
-    }
-}
-
-function slideAllText() {
-    for (let i = 0; i < destinationBodies.length; i++) {
-        destinationBodies[i].classList.add('js-slide-in');
-    }
-}
+    navBtn.addEventListener('click', toggleNav);
+    window.addEventListener('load', function() {
+        h1.classList.add('js-slide-in');
+    });
+})();
 
 function toggleNav() {
     header.classList.toggle('js-nav-open');
@@ -104,10 +70,8 @@ function closeNav() {
 
 function autoCloseNav(e) {
     if (header.classList.contains('js-nav-open') &&
-        (introContent.contains(e.target) ||
-         historyContent.contains(e.target) ||
-         destinationsContent.contains(e.target))) {
-             toggleNav();
+        !header.contains(e.target)) {
+            toggleNav();
     }
 }
 
@@ -123,75 +87,108 @@ function supportsObservers() {
            'isIntersecting' in window.IntersectionObserverEntry.prototype;
 }
 
-function createObservers() {
-    (function() {
-        let io = null;
-        const navList_RO = new ResizeObserver(create_h1_IO);
-        navList_RO.observe(navList);
+function createH1Observers() {
+    /* we need a resize observer because when the root font size changes, the
+    header height changes as well. Therefore the header will now intersect h1
+    sooner and we need a new intersection observer for h1 */
 
-        function create_h1_IO(entries, observer) {
-            if (io) {
-                io.unobserve(h1);
-            }
-            const headerHeight = header.getBoundingClientRect().height;
-            const options = {
-                threshold: 1,
-                rootMargin: -headerHeight + 'px 0px 0px 0px'
-            };
-            io = new IntersectionObserver(changeHeader, options);
-            io.observe(h1);
+    let io = null;
+    const navList_RO = new ResizeObserver(create_h1_IO);
+    navList_RO.observe(navList);
+
+    function create_h1_IO(entries, observer) {
+        if (io) {
+            io.unobserve(h1);
         }
-    })();
-    
-    (function() {
+        const headerHeight = header.getBoundingClientRect().height;
         const options = {
-            rootMargin: '-50% 0px -50% 0px'
+            threshold: 1,
+            rootMargin: -headerHeight + 'px 0px 0px 0px'
         };
-        const io = new IntersectionObserver(underlineCurrentLink, options);
-        io.observe(introContent);
-        io.observe(historyContent);
-        io.observe(destinationsContent);
-    })();
-    
-    (function() {
-        const options = {
-            threshold: .8
-        };
-        const io = new IntersectionObserver(loadImg, options);
-        destinations.forEach(function(x, i) {
-            io.observe(x);
-        });
-    })();
-    
-    (function() {
-        const options = {
-            threshold: .5
-        };
-        const io = new IntersectionObserver(slideText, options);
-        destinations.forEach(function(x, i) {
-            io.observe(x);
-        });
-    })();
+        io = new IntersectionObserver(changeHeader, options);
+        io.observe(h1);
+    }
+
+    function changeHeader(entries, observer) {
+        for (let i = 0; i < entries.length; i++) {
+            if (entries[i].isIntersecting) {
+                if (!header.classList.contains('js-nav-open')) {
+                    header.classList.add('js-bg-transparent');
+                }
+                header.classList.remove('box-shadow');
+                header.classList.add('js-above-h1');
+            } else {
+                if (!header.classList.contains('js-nav-open')) {
+                    header.classList.add('box-shadow');
+                }
+                header.classList.remove('js-bg-transparent');
+                header.classList.remove('js-above-h1');
+            }
+        }
+    }
 }
 
-(function() {
-    if (mqList.addEventListener) {
-        mqList.addEventListener('change', closeNavOnResize);
-    } else {
-        mqList.addListener(closeNavOnResize);
+function createSectionObservers() {
+    const options = {
+        rootMargin: '-50% 0px -50% 0px'
+    };
+    const io = new IntersectionObserver(underlineCurrentLink, options);
+    io.observe(introContent);
+    io.observe(historyContent);
+    io.observe(destinationsContent);
+
+    function underlineCurrentLink(entries, observer) {
+        for (let i = 0; i < entries.length; i++) {
+            if (entries[i].isIntersecting) {
+                let id = entries[i].target.parentNode.id;
+                let currentUnderlined = navList.querySelector('.current-link');
+                let newUnderlined = navList.querySelector('[href="#' + id + '"]');
+                if (currentUnderlined) {
+                    currentUnderlined.classList.remove('current-link');
+                }
+                newUnderlined.classList.add('current-link');
+            }
+        }
     }
-    window.addEventListener('click', autoCloseNav);
-    if (supportsObservers()) {
-        createObservers();
-    } else {
-        loadAllImages();
-        slideAllText();
-    }
-    for (let i = 0; i < navLinks.length; i++) {
-        navLinks[i].addEventListener('click', closeNav);
-    }
-    navBtn.addEventListener('click', toggleNav);
-    window.addEventListener('load', function() {
-        h1.classList.add('js-slide-in');
+}
+
+function createDestinationInfoObservers() {
+    const options = {
+        threshold: .5
+    };
+    const io = new IntersectionObserver(slideText, options);
+    destinations.forEach(function(x, i) {
+        io.observe(x);
     });
-})();
+
+    function slideText(entries, observer) {
+        for (let i = 0; i < entries.length; i++) {
+            if (entries[i].isIntersecting) {
+                let planetBody = entries[i].target.querySelector('.destination-body');
+                planetBody.classList.add('js-slide-in');
+                observer.unobserve(entries[i].target);
+            }
+        }
+    }
+}
+
+function createDestinationPhotoObservers() {
+    const options = {
+        threshold: .8
+    };
+    const io = new IntersectionObserver(loadPhoto, options);
+    destinations.forEach(function(x, i) {
+        io.observe(x);
+    });
+
+    function loadPhoto(entries, observer) {
+        for (let i = 0; i < entries.length; i++) {
+            if (entries[i].isIntersecting) {
+                let img = entries[i].target.querySelector('img');
+                img.src = img.getAttribute('data-src');
+                img.classList.add('js-loaded-img');
+                observer.unobserve(entries[i].target);
+            }
+        }
+    }
+}
